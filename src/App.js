a59ecf573d68dfa5aa9def13fd40c97a85f00909
Ali Karakoç <bykrkc@gmail.com>
2022-02-05 18:27:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 
 function App() {
-  const baslik = "Ali Karakoç";
-  let okunmaSayisi = 300;
   return (
     <div className="App">
       <div className="content">
-        <h1>{baslik}</h1>
-        <p>Okunma Sayısı : {okunmaSayisi}</p>
       </div>
     </div>
   );
